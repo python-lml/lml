0ab0a1a50a9ad4554658c6383b17e4f329c82913
chfw <wangc_2011@hotmail.com>
2018-11-06 18:07:27 +0000

    :green_heart: use flake8 3.x syntax to ignore flake8 warnings

--- examples/v2/robotchef_api/robotchef_api/__init__.py
+++ examples/v2/robotchef_api/robotchef_api/__init__.py
@@ -1,12 +1,12 @@
 from lml.loader import scan_plugins
-from robotchef_api.plugin import (
+from robotchef_api.plugin import (  # noqa: F401
     CuisineManager,
     NoChefException,
-)  # flake8: noqa
+)
 
 
 BUILTINS = ["robotchef_api.robot_cuisine"]
 
 
-scan_plugins("robotchef_", __path__, white_list=BUILTINS)
+scan_plugins("robotchef_", __path__, white_list=BUILTINS)  # noqa: F821
 cuisine_manager = CuisineManager()
