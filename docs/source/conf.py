ff205d34a8e961d5671cd96ffdcc5f2a7f230dc9
chfw <wangc_2011@hotmail.com>
2019-01-04 12:04:09 +0000

    :hammer: add tests and docs for package validation. resolve #9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,9 +28,9 @@ copyright = u'2017-2018 Onni Software Ltd.'
 author = u'C.W.'
 
 # The short X.Y version
-version = u'0.0.5'
+version = u'0.0.8'
 # The full version, including alpha/beta/rc tags
-release = u'0.0.5'
+release = u'0.0.8'
 
 
 # -- General configuration ---------------------------------------------------
