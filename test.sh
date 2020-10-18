413cf633cb39fb0e88710a1c2867bba61a1e9204
chfw <wangc_2011@hotmail.com>
2020-10-18 21:23:59 +0100

    :bug: update coverage module

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 pip freeze
-pytest tests --verbosity=3 --cov=moban --doctest-glob=*.rst && flake8 . --exclude=.moban.d,docs,setup.py --builtins=unicode,xrange,long
+pytest tests --verbosity=3 --cov=lml --doctest-glob=*.rst && flake8 . --exclude=.moban.d,docs,setup.py --builtins=unicode,xrange,long
