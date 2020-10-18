d6d05b18a02e44db89c527dc0ed2198ad6022cf4
chfw <wangc_2011@hotmail.com>
2020-10-18 22:22:59 +0100

    :green_heart: update tests

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 pip freeze
+cd tests/test_plugin
+python setup.py install
+cd -
 pytest tests --verbosity=3 --cov=lml --doctest-glob=*.rst && flake8 . --exclude=.moban.d,docs,setup.py --builtins=unicode,xrange,long
